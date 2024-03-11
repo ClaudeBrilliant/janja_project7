0e1e55ce212c3e8f9a9b1df92d28c802a71f4549
ClaudeBrilliant <claudennyongesa7@gmail.com>
2024-03-11 21:02:08 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('paragraph').innerText="hello world";
+
+const imgElement = document.createElement('img');
+
+imgElement.src = 'image.jpg';
+
+ imgElement.alt = "clyde's image"
+
+document.body.appendChild(imgElement);
